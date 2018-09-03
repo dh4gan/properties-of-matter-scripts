1731be5b9ccf63ad01e557d97f4cf089c8ed8819
Duncan Forgan <dhf3@st-andrews.ac.uk>
2018-09-03 10:35:46 +0100

    (lennard-jones) Add repulsive/attractive force methods and figures, tidy up formatting

--- lennard_jones.py
+++ lennard_jones.py
@@ -24,9 +24,21 @@ def lennard_jones(x,sigma,epsilon):
     return V
 
 def force(x,sigma,epsilon):
-    '''Compute the force from the Lennard_Jones potential (-grad potential)'''
+    '''Compute the force from the Lennard-Jones potential (-grad potential)'''
 
-    F = 4*epsilon*(12*np.power(sigma/x,13) - 6*np.power(sigma/x,7))
+    F = 4.0*epsilon*(12.0*np.power(sigma/x,13) - 6.0*np.power(sigma/x,7))
+    return F
+
+def rep_force(x,sigma,epsilon):
+    '''Compute the repulsive force from the Lennard-Jones potential'''
+
+    F = 4.0*epsilon*12.0*np.power(sigma/x,13)
+    return F
+
+def att_force(x,sigma,epsilon):
+    '''Compute the force from the Lennard-Jones potential (-grad potential)'''
+    
+    F = -4*epsilon*6*np.power(sigma/x,7)
     return F
 
 
@@ -36,6 +48,9 @@ def force(x,sigma,epsilon):
 
 sigma = 1.5
 epsilon = 100.0
+
+# Equilibrium separation = force is zero
+equilibriumsep = np.power(2,1.0/6.0)*sigma
 
 # Potential minimum is at 2^{1/6}*sigma
 
@@ -52,9 +67,13 @@ tmin = 0.0 # beginning time
 tmax = 10.0 # end time
 t = np.linspace(tmin,tmax, num=500) # Range of points between beginning and end time
 
-# Compute potential at all x points
-domain = np.linspace(0.1,5*sigma, num=100) # Range of position points
-potential = lennard_jones(domain,sigma,epsilon)
+# Compute potential and force at all x points in the domain
+domain = np.linspace(0.1,5*sigma, num=200) # Range of position points
+potential_domain = lennard_jones(domain,sigma,epsilon)
+force_domain = force(domain,sigma,epsilon)
+rep_force_domain = rep_force(domain,sigma,epsilon)
+att_force_domain = att_force(domain,sigma,epsilon)
+
 
 # set up and perform integration
 y0 = [v0,x0]
@@ -64,7 +83,7 @@ intgr = odeint(dydt, y0,t, args=(sigma,epsilon))
 v = intgr[:,0]
 x = intgr[:,1]
 
-# Plot results
+# Plot Position vs Time diagram for particle in potential
 fig1 = plt.figure(1)
 ax1 = fig1.add_subplot(111)
 ax1.set_xlabel('Position')
@@ -72,13 +91,47 @@ ax1.set_ylabel('Time')
 ax1.plot(x,t, label='position')
 ax1.legend()
 
+
+# Plot Lennard-Jones Potential
 fig2 = plt.figure(2)
 ax2 = fig2.add_subplot(111)
 ax2.set_ylim((-2.0*epsilon, 10.0*epsilon))
-ax2.set_xlabel('Position')
-ax2.set_ylabel('Potential')
-ax2.plot(domain,potential)
+ax2.set_xlabel('Position',fontsize=18)
+ax2.set_ylabel('Lennard-Jones Potential',fontsize=18)
+ax2.axhline(-epsilon, color='orange', linestyle='dashed', label='$\epsilon$')
+ax2.axhline(0.0,color='black', linewidth=0.5)
+ax2.axvline(equilibriumsep, color='green', linestyle='dotted', label='d')
 
+ax2.plot(domain,potential_domain)
+ax2.legend(fontsize=18)
+
+fig2.savefig('LJ_potential.png')
+
+# Plot Lennard-Jones Force
+fig3 = plt.figure(3)
+ax3 = fig3.add_subplot(111)
+ax3.set_ylim((-4.0*epsilon,10.0*epsilon))
+ax3.set_xlabel('Position',fontsize=18)
+ax3.set_ylabel('Lennard-Jones Force',fontsize=18)
+ax3.plot(domain,force_domain, label='Total Force')
+ax3.axvline(equilibriumsep, color='green', linestyle='dotted', label='d')
+ax3.legend(fontsize=18)
+
+fig3.savefig('LJ_force.png')
+
+# Plot attractive/repulsive components of Lennard-Jones Force
+fig4 = plt.figure(4)
+ax4 = fig4.add_subplot(111)
+ax4.set_ylim((-1.5*epsilon,1.5*epsilon))
+ax4.set_xlim(0.0,3.0*sigma)
+ax4.set_xlabel('Position',fontsize=18)
+ax4.set_ylabel('Repulsive/Attractive Force',fontsize=18)
+ax4.plot(domain,rep_force_domain[rep_force_domain>0.0],label='Repulsive')
+ax4.axhline(0.0, color='black', linestyle='dotted')
+ax4.plot(domain,att_force_domain,label='Attractive')
+ax4.legend(fontsize=18, loc='lower left')
+
+fig4.savefig('LJ_forcecomponents.png')
 
 plt.show()
 
